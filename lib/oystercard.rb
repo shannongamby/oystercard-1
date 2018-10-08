9c95e9d88f266445c01f45fdfe89a03f0e7ffd3e
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-10-08 15:47:33 +0100

    step 7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -9,8 +9,12 @@ class Oystercard
   end
 
   def topup(amount)
-    raise "Cannot topup #{amount}: maximum balance of £90" if (90 - @balance) < amount
+    raise "Cannot topup £#{amount}: maximum balance of £90" if (90 - @balance) < amount
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,7 +17,13 @@ describe Oystercard do
   end
 
   it 'cannot store a balance above £90' do
-    expect { @oyster.topup(91) }.to raise_error "Exceeded maximum balance of £90"
+    expect { @oyster.topup(91) }.to raise_error "Cannot topup £91: maximum balance of £90"
+  end
+
+  it 'will deduct a fare' do
+    @oyster.topup(10)
+    @oyster.deduct(5)
+    expect(@oyster.balance).to eq (5)
   end
 
 end
